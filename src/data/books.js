796d7177c60c2ec30c17cb7b191e458196c8e14e
Damian Figueroa <damy.mtl@gmail.com>
2023-07-07 13:57:19 -0300

    feat: i move books to data nad added page 404

---
+++ src/data/books.js
@@ -0,0 +1,27 @@
+export const books = [
+    {
+        id: 'clean-code',
+        title: 'Programador Pragmatico',
+        img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1401432508i/4099.jpg'
+    },
+    {
+        id: 'clean-code2',
+        title: 'Programador Pragmatico',
+        img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1401432508i/4099.jpg'
+    },
+    {
+        id: 'clean-code3',
+        title: 'Programador Pragmatico',
+        img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1401432508i/4099.jpg'
+    },
+    {
+        id: 'clean-code2',
+        title: 'Programador Pragmatico',
+        img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1401432508i/4099.jpg'
+    },
+    {
+        id: 'clean-code2',
+        title: 'Programador Pragmatico',
+        img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1401432508i/4099.jpg'
+    }
+]
