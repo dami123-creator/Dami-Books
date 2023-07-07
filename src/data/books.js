7e785892df62733ae629df871e7708275c5c6deb
Damian Figueroa <damy.mtl@gmail.com>
2023-07-07 14:26:23 -0300

    feat: [books] added

--- src/data/books.js
+++ src/data/books.js
@@ -2,26 +2,36 @@ export const books = [
     {
         id: 'clean-code',
         title: 'Programador Pragmatico',
-        img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1401432508i/4099.jpg'
+        img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1401432508i/4099.jpg',
+        opinion: 'en este libro podemos observer muchas cosas que no se',
+        author: 'el mismisimo'
     },
     {
         id: 'clean-code2',
         title: 'Programador Pragmatico',
-        img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1401432508i/4099.jpg'
+        img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1401432508i/4099.jpg',
+        opinion: 'en este libro podemos observer muchas cosas que no se',
+        author: 'el mismisimo'
     },
     {
         id: 'clean-code3',
         title: 'Programador Pragmatico',
-        img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1401432508i/4099.jpg'
+        img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1401432508i/4099.jpg',
+        opinion: 'en este libro podemos observer muchas cosas que no se',
+        author: 'el mismisimo'
     },
     {
         id: 'clean-code2',
         title: 'Programador Pragmatico',
-        img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1401432508i/4099.jpg'
+        img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1401432508i/4099.jpg',
+        opinion: 'en este libro podemos observer muchas cosas que no se',
+        author: 'el mismisimo'
     },
     {
         id: 'clean-code2',
         title: 'Programador Pragmatico',
-        img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1401432508i/4099.jpg'
+        img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1401432508i/4099.jpg',
+        opinion: 'en este libro podemos observer muchas cosas que no se',
+        author: 'el mismisimo'
     }
 ]
